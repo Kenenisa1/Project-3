c8a7aa920b96c43280916652ba964ce6f68a1bb4
KENENISA MB <kenenisamiesob@Gmail.com>
2025-04-19 20:52:39 +0000

    mapping practice on App.jsx and css are added

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import React from "react"
+const Products =[
+  { name: 'Car', year: 2024},
+  { name:  'Bus', year: 2024},
+  { name: 'Sheep', year: 2024},
+  { name: 'Airplane', year: 2024},
+  { name: 'Train', year: 2024}
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const ProductList=Products.map(product=> 
+    <li>{product.name}</li>
+  );
 
   return (
     <>
-
+      <h1>List of Our Products</h1>
+      <hr />
+      <ul>{ProductList} </ul>
     </>
   )
 }
