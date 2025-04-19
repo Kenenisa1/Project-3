f1b0ecb76d2f8763f929ec2c7113d12c6a22c339
KENENISA MB <kenenisamiesob@Gmail.com>
2025-04-19 21:26:22 +0000

    Image linking and other practices

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -6,10 +6,14 @@ const Products =[
   { name: 'Airplane', year: 2024},
   { name: 'Train', year: 2024}
 ]
+const images={
+URLs: './assets/coder.jpg',
+name: 'Coding Man'
+}
 
 function App() {
   const ProductList=Products.map(product=> 
-    <li>{product.name}</li>
+    <li>{product.name} Product year {product.year}</li>
   );
 
   return (
@@ -17,6 +21,10 @@ function App() {
       <h1>List of Our Products</h1>
       <hr />
       <ul>{ProductList} </ul>
+      <div>
+        <h3>Coding Man</h3>
+      <img src={images.URLs} alt={images.name} />
+      </div>
     </>
   )
 }
